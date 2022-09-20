d5a4d3a24ea77346136fba552288e504409e10f8
Rasul <rasul.yunusov282750@gmail.com>
2022-09-20 22:08:04 +0300

    update: gitignore

--- mixBytes/testAudit/contracts/VBeacon.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
-
-contract VBeacon is UpgradeableBeacon {
-    constructor(address implementation_) UpgradeableBeacon(implementation_) {}
-}
